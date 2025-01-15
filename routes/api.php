bee5fa704ffc173bcfb8494a53990eb369327ff2
rickyrich88 <r_shin2@u.pacific.edu>
2025-01-14 21:57:22 -0800

    Create api routes

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,29 @@
 <?php
 
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('projects')->group(function () {
+    Route::get('/', [ProjectController::class, 'index']);
+    Route::get('/{id}', [ProjectController::class, 'show']);
+
+    Route::post('/', [ProjectController::class, 'store']);
+
+    Route::put('/{id}', [ProjectController::class, 'update']);
+
+    Route::delete('/{id}', [ProjectController::class, 'destroy']);
+    
+    Route::get('/{project_id}/tasks', [TaskController::class, 'showProjectTasks']);
+    Route::post('/{project_id}/tasks', [TaskController::class, 'storeForProject']);
+});
+
+Route::prefix('tasks')->group(function () {
+    Route::get('/', [TaskController::class, 'index']);
+    Route::get('/{id}', [TaskController::class, 'show']);
+
+    Route::put('/{id}', [TaskController::class, 'update']);
+
+    Route::delete('/{id}', [TaskController::class, 'destroy']);
+
+});
