a93f7a77fcb72c9a3c6780cf4cd2355a991debea
rickyrich88 <r_shin2@u.pacific.edu>
2025-01-15 14:42:12 -0800

    Catch wrong method for api endpoint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\JsonResponseForApi;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -18,6 +19,12 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Api endpoint not found'
+            ], 404);
+        });
+        $exceptions->renderable(function (MethodNotAllowedHttpException $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'Api endpoint not found'
