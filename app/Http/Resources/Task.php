da17f516cadab4faaca6bb617e0146f188f305da
rickyrich88 <r_shin2@u.pacific.edu>
2025-01-14 22:02:12 -0800

    Create Project, Task Resources

---
+++ app/Http/Resources/Task.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Task extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'project_id' => $this->project_id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'assigned_to' => $this->assigned_to,
+            'due_date' => $this->due_date,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
